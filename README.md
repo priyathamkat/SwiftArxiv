2df07e6a38f2420b7724327ad097443b75ae0cea
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-25 20:51:18 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftArxiv
+
+A description of this package.
---
+++ Tests/SwiftArxivTests/SwiftArxivTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftArxiv
+
+final class SwiftArxivTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftArxiv().text, "Hello, World!")
+    }
+}
